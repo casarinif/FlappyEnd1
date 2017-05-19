49c143c71689b3807a5baad30fc35fc7756a33dc
Eduardo Casarini <casarinif@gmail.com>
2017-05-19 09:53:04 -0300

    Ajuste da camera Limpado menoria

--- core/src/com/cursoandroid/flappybird/FlappyBird.java
+++ core/src/com/cursoandroid/flappybird/FlappyBird.java
@@ -3,6 +3,7 @@ package com.cursoandroid.flappybird;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
@@ -97,6 +98,10 @@ public class FlappyBird extends ApplicationAdapter {
 
     @Override
     public void render() {
+
+        camera.update();
+        //limpar frames anteriores liberando menorias
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
 
         deltaTime = Gdx.graphics.getDeltaTime();
         // Movimento do passaros
